205e0eca6ef7006a3218aab7d35addaf1e3815b3
jessy3ric <jess88230@gmail.com>
2026-02-16 21:40:08 +0100

    introduce filehelper to fetch the prompts

---
+++ AgenticDevelopmentExercise/Helpers/FileHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace AgenticDevelopmentExercise.Helpers
+{
+    internal class FileHelper
+    {
+        private string[] _fileNames = { "Prompts/AnalyzerAgentPrompt.txt", "Prompts/QueryAgentPrompt.txt", "Prompts/LawyerAgentPrompt.txt" };
+
+        public async Task<IDictionary<string, string>> GetSystemPrompts()
+        {
+            IDictionary<string, string> promptMap = new Dictionary<string, string>();
+            try
+            {
+                promptMap = await LoadPromptsAsDictionaryAsync();
+            }
+             catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            return promptMap;
+        }
+
+        /// <summary>
+        /// Loads files into a Dictionary where Key = Filename, Value = Content.
+        /// </summary>
+        private async Task<Dictionary<string, string>> LoadPromptsAsDictionaryAsync()
+        {
+            var absolutePaths = _fileNames.Select(p => Path.GetFullPath(p)).ToArray();
+
+            // Create the tasks for reading
+            var tasks = absolutePaths.Select(async path => new
+            {
+                Name = Path.GetFileName(path),
+                Content = await File.ReadAllTextAsync(path)
+            });
+
+            // Wait for all reads to complete
+            var results = await Task.WhenAll(tasks);
+
+            return results.ToDictionary(x => x.Name, x => x.Content);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Legal Agentic Script
 This c# script aims to provide a legitimate legal document to a user query
+
+## Base configurations
+You need to get two api keys and store them in the user secrets:
+
+1. "braveApiKey": this api key can be fetched on https://brave.com/search/api/ ; this api will be used by one of the agent to make queries on internet.
+2. "openAiApiKey": this api key can be fetched on https://platform.openai.com/api-keys ; it will be used to power the agents with "gpt-4o-mini".
+
+These two keys are mandatory for the script to fully work.
+
+## Main library used
+Semantic-Kernel
