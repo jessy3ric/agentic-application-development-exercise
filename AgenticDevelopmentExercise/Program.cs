e9093f719736628e7c22bc728168b3ae2803bf6e
jessy3ric <jess88230@gmail.com>
2026-02-16 21:12:42 +0100

    create base project with credential loading and useful nuget packages

---
+++ AgenticDevelopmentExercise/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+     }
+}
